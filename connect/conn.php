1435f727992e6baaf299b40b005f5f5f574311ac
uengHZ <uenghernama@gmail.com>
2020-07-16 21:34:42 +0700

    update connection

--- connect/conn.php
+++ connect/conn.php
@@ -2,19 +2,14 @@
 	set_time_limit(0);
 	ini_set('memory_limit', '-1');
 	
-	
-
 	$serverName = "localhost,1433"; 
-	$connectionInfo = array( "Database"=>"FDKSYS20","uid"=>"sa","pwd"=>"P@ssw0rd");
+	$connectionInfo = array( "Database"=>"FDKSYS20","Uid"=>"sa","PWD"=>"accpac","TraceOn"=>"0");
 	$connect = sqlsrv_connect( $serverName, $connectionInfo);
-	
+
 	if($connect) {
 		$varConn = "Y";
 	}else{
 		$varConn = "N";
 		die(print_r(sqlsrv_errors(),true));
 	}
-	
-
-
 ?>
