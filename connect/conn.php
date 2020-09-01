10472df667e9994b81735ad201ae7b5cb8c99234
uengHZ <uenghernama@gmail.com>
2020-09-02 00:50:52 +0700

    commit baru

--- connect/conn.php
+++ connect/conn.php
@@ -1,19 +1,13 @@
 <?php
+	// error_reporting(0);
 	set_time_limit(0);
 	ini_set('memory_limit', '-1');
-	// $serverName = "172.23.225.113,1433";
-	// $connectionInfo = array( "Database"=>"FDKSYS20","uid"=>"sa","pwd"=>"accpac","TraceOn"=>"0");
+	
+	$serverName = "localhost,1433"; 
 
-	// $serverName = "localhost,1433";
-	// $connectionInfo = array( "Database"=>"FDKSYS20","uid"=>"sa","pwd"=>"P@ssw0rd","TraceOn"=>"0");
-
-	$serverName = "localhost,1433";
 	$connectionInfo = array( "Database"=>"FDKSYS20","uid"=>"sa","pwd"=>"P@ssw0rd","TraceOn"=>"0");
-	$connect = sqlsrv_connect( $serverName, $connectionInfo);
 
-	// $connectionInfo = array("UID" => "BUMIKAYA", "pwd" => "AdminFDKI4212", "Database" => "FDKSYS20", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
-    // $serverName = "tcp:fdksys12.database.windows.net,1433";
-    // $connect = sqlsrv_connect($serverName, $connectionInfo);
+	$connect = sqlsrv_connect( $serverName, $connectionInfo);	
 
 	if($connect) {
 		$varConn = "Y";
@@ -22,4 +16,3 @@
 		die(print_r(sqlsrv_errors(),true));
 	};
 ?>
-
